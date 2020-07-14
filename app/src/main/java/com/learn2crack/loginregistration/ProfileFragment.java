81e206247dbbf7379f17c4fef950489b14f13bd2
argaed <argaed@gmail.com>
2020-07-14 17:38:46 -0500

    add onActivityResult in activity

--- app/src/main/java/com/learn2crack/loginregistration/ProfileFragment.java
+++ app/src/main/java/com/learn2crack/loginregistration/ProfileFragment.java
@@ -31,10 +31,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ProfileFragment extends Fragment implements View.OnClickListener {
 
-    private TextView tv_name,tv_email,tv_message,txt_lectura_qr;
+    private TextView tv_name, tv_email, tv_message, txt_lectura_qr;
     private SharedPreferences pref;
-    private AppCompatButton btn_change_password,btn_logout,btn_estatus_guia;
-    private EditText et_old_password,et_new_password;
+    private AppCompatButton btn_change_password, btn_logout, btn_estatus_guia;
+    private EditText et_old_password, et_new_password;
     private AlertDialog dialog;
     private ProgressBar progress;
 
@@ -42,44 +42,43 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_profile,container,false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
         initViews(view);
         return view;
     }
-
 
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
 
         pref = getActivity().getPreferences(0);
-        tv_name.setText("Usuario: "+pref.getString(Constants.EMAIL,""));
-        tv_email.setText("Ubicaciòn: "+pref.getString(Constants.NAME,""));
+        tv_name.setText("Usuario: " + pref.getString(Constants.EMAIL, ""));
+        tv_email.setText("Ubicaciòn: " + pref.getString(Constants.NAME, ""));
 
     }
 
-    private void initViews(View view){
+    private void initViews(View view) {
 
-        tv_name = (TextView)view.findViewById(R.id.tv_name);
-        txt_lectura_qr= (TextView)view.findViewById(R.id.txt_lectura_qr);
-        tv_email = (TextView)view.findViewById(R.id.tv_email);
-        btn_change_password = (AppCompatButton)view.findViewById(R.id.btn_chg_password);
-        btn_estatus_guia = (AppCompatButton)view.findViewById(R.id.btn_estatus_guia);
-        btn_logout = (AppCompatButton)view.findViewById(R.id.btn_logout);
+        tv_name = (TextView) view.findViewById(R.id.tv_name);
+        txt_lectura_qr = (TextView) view.findViewById(R.id.txt_lectura_qr);
+        tv_email = (TextView) view.findViewById(R.id.tv_email);
+        btn_change_password = (AppCompatButton) view.findViewById(R.id.btn_chg_password);
+        btn_estatus_guia = (AppCompatButton) view.findViewById(R.id.btn_estatus_guia);
+        btn_logout = (AppCompatButton) view.findViewById(R.id.btn_logout);
         btn_change_password.setOnClickListener(this);
         btn_logout.setOnClickListener(this);
         btn_estatus_guia.setOnClickListener(this);
     }
 
-    private void showDialog(){
+    private void showDialog() {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.dialog_change_password, null);
-        et_old_password = (EditText)view.findViewById(R.id.et_old_password);
-        et_new_password = (EditText)view.findViewById(R.id.et_new_password);
-        tv_message = (TextView)view.findViewById(R.id.tv_message);
-        progress = (ProgressBar)view.findViewById(R.id.progress);
+        et_old_password = (EditText) view.findViewById(R.id.et_old_password);
+        et_new_password = (EditText) view.findViewById(R.id.et_new_password);
+        tv_message = (TextView) view.findViewById(R.id.tv_message);
+        progress = (ProgressBar) view.findViewById(R.id.progress);
         builder.setView(view);
         builder.setTitle("Cambiar Contraseña");
         builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
@@ -97,27 +96,27 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         dialog = builder.create();
         dialog.show();
         dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String old_password = et_old_password.getText().toString();
-                    String new_password = et_new_password.getText().toString();
-                    if(!old_password.isEmpty() && !new_password.isEmpty()){
+            @Override
+            public void onClick(View v) {
+                String old_password = et_old_password.getText().toString();
+                String new_password = et_new_password.getText().toString();
+                if (!old_password.isEmpty() && !new_password.isEmpty()) {
 
-                        progress.setVisibility(View.VISIBLE);
-                        changePasswordProcess(pref.getString(Constants.EMAIL,""),old_password,new_password);
+                    progress.setVisibility(View.VISIBLE);
+                    changePasswordProcess(pref.getString(Constants.EMAIL, ""), old_password, new_password);
 
-                    }else {
+                } else {
 
-                        tv_message.setVisibility(View.VISIBLE);
-                        tv_message.setText("Fields are empty");
-                    }
+                    tv_message.setVisibility(View.VISIBLE);
+                    tv_message.setText("Fields are empty");
                 }
-            });
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.btn_chg_password:
                 showDialog();
                 break;
@@ -132,7 +131,7 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
 
     //de aqui hacia abajo este es el codigo que segun yo hace lo de la lectura de QR y
     // arroja el resultado pero no me da nada
-    public void leerQr(){
+    public void leerQr() {
         IntentIntegrator integrator = new IntentIntegrator(getActivity());
         integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
         integrator.setPrompt("Scan!!");
@@ -147,10 +146,10 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         super.onActivityResult(requestCode, resultCode, data);
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null)
-            if(result.getContents() != null) {
+            if (result.getContents() != null) {
                 //(txt_lectura_qr.setText("El codigo qr es:\n" + result.getContents());
                 separacionQr(result.getContents());
-            }else{
+            } else {
                 txt_lectura_qr.setText("Error De Lectura Volver A Leer Al Codigo QR");
             }
     }
@@ -162,7 +161,7 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
         String[] separated = text.split("\\|");
         String codigo = separated[0];
         Log.e("codigo", codigo);
-        this.codigo=codigo;
+        this.codigo = codigo;
         txt_lectura_qr.setText("ID_GUIA:" + codigo);
     }
 
@@ -172,23 +171,23 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
 
     private void logout() {
         SharedPreferences.Editor editor = pref.edit();
-        editor.putBoolean(Constants.IS_LOGGED_IN,false);
-        editor.putString(Constants.EMAIL,"");
-        editor.putString(Constants.NAME,"");
-        editor.putString(Constants.UNIQUE_ID,"");
+        editor.putBoolean(Constants.IS_LOGGED_IN, false);
+        editor.putString(Constants.EMAIL, "");
+        editor.putString(Constants.NAME, "");
+        editor.putString(Constants.UNIQUE_ID, "");
         editor.apply();
         goToLogin();
     }
 
-    private void goToLogin(){
+    private void goToLogin() {
 
         Fragment login = new LoginFragment();
         FragmentTransaction ft = getFragmentManager().beginTransaction();
-        ft.replace(R.id.fragment_frame,login);
+        ft.replace(R.id.fragment_frame, login);
         ft.commit();
     }
 
-    private void changePasswordProcess(String email,String old_password,String new_password){
+    private void changePasswordProcess(String email, String old_password, String new_password) {
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(Constants.BASE_URL)
@@ -211,13 +210,13 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
             public void onResponse(Call<ServerResponse> call, retrofit2.Response<ServerResponse> response) {
 
                 ServerResponse resp = response.body();
-                if(resp.getResult().equals(Constants.SUCCESS)){
+                if (resp.getResult().equals(Constants.SUCCESS)) {
                     progress.setVisibility(View.GONE);
                     tv_message.setVisibility(View.GONE);
                     dialog.dismiss();
                     Snackbar.make(getView(), resp.getMessage(), Snackbar.LENGTH_LONG).show();
 
-                }else {
+                } else {
                     progress.setVisibility(View.GONE);
                     tv_message.setVisibility(View.VISIBLE);
                     tv_message.setText(resp.getMessage());
@@ -228,7 +227,7 @@ public class ProfileFragment extends Fragment implements View.OnClickListener {
             @Override
             public void onFailure(Call<ServerResponse> call, Throwable t) {
 
-                Log.d(Constants.TAG,"failed");
+                Log.d(Constants.TAG, "failed");
                 progress.setVisibility(View.GONE);
                 tv_message.setVisibility(View.VISIBLE);
                 tv_message.setText(t.getLocalizedMessage());
